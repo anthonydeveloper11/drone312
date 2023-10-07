274702ca6f114448260a765d5bb49a995cf5224b
raju sharma <hari.freelancer.world@gmail.com>
2023-10-07 12:37:49 +0530

    added some changes

---
+++ src/app/common/const/constant.ts
@@ -0,0 +1,124 @@
+export const products = {
+    "menu": [
+      {
+        "id": "uav",
+        "label": "UAV",
+        "description": "",
+        "submenu": [
+          {
+            "id": "surveying",
+            "label": "Surveying and Mapping",
+            "description": "",
+            "submenu": [
+              {"id": "trinity-f90", "label": "Trinity F90+", "description": ""},
+              {"id": "rc08", "label": "RC08", "description": ""}
+            ]
+          },
+          {
+            "id": "isr",
+            "label": "ISR",
+            "description": "",
+            "submenu": [
+              {"id": "vector", "label": "Vector", "description": ""},
+              {"id": "scorpion", "label": "Scorpion", "description": ""},
+              {"id": "rc08-isr", "label": "RC08", "description": ""}
+            ]
+          },
+          {
+            "id": "multi-purpose",
+            "label": "Multi-purpose",
+            "description": "",
+            "submenu": [
+              {"id": "rc08-multi", "label": "RC08", "description": ""},
+              {"id": "velos-v3", "label": "Velos V3", "description": ""}
+            ]
+          }
+        ]
+      },
+      {
+        "id": "lidar",
+        "label": "LIDAR",
+        "description": "",
+        "submenu": [
+          {
+            "id": "aerial",
+            "label": "Aerial",
+            "description": "",
+            "submenu": [
+              {"id": "yellowscan-voyager", "label": "yellowscan--> Voyager", "description": ""},
+              {"id": "yellowscan-fly-drive", "label": "Yellowscan-->Fly&Drive", "description": ""},
+              {"id": "vx-20", "label": "VX-20 series[100,200]", "description": ""},
+              {"id": "vx-15", "label": "Vx-15 series[100,300]", "description": ""},
+              {"id": "surveyor-ultra", "label": "Surveyor Ultra", "description": ""},
+              {"id": "explorer", "label": "Explorer", "description": ""},
+              {"id": "mapper", "label": "Mapper", "description": ""},
+              {"id": "mapper-plus", "label": "Mapper+", "description": ""}
+            ]
+          },
+          {
+            "id": "terrestrial",
+            "label": "Terrestrial",
+            "description": "",
+            "submenu": [
+              {"id": "z-f-5016r", "label": "3d --> Z+F IMAGER®5016r", "description": ""},
+              {"id": "z-f-5016a", "label": "3d--> Z+F IMAGER®5016A", "description": ""},
+              {"id": "z-f-9012", "label": "2d --> Z+F PROFILER® 9012", "description": ""},
+              {"id": "z-f-9012a", "label": "2d--> Z+F PROFILER® 9012 A", "description": ""},
+              {"id": "z-f-9012m", "label": "2d--> Z+F PROFILER® 9012 M", "description": ""},
+              {"id": "z-f-9020", "label": "2d --> Z+F PROFILER® 9020", "description": ""},
+              {"id": "z-f-9020c", "label": "2d --> Z+F PROFILER® 9020 c", "description": ""}
+            ]
+          },
+          {
+            "id": "underground",
+            "label": "Underground",
+            "description": "",
+            "submenu": [
+              {"id": "z-f-5006ex", "label": "Z+F IMAGER® 5006EX", "description": ""},
+              {"id": "gsm-16", "label": "cavity monitoring system --> GSM-16", "description": ""},
+              {"id": "geo-sight-scout", "label": "GeoSight Scout", "description": ""},
+              {"id": "geo-sight-nx150", "label": "GeoSight NX150", "description": ""}
+            ]
+          }
+        ]
+      },
+      {
+        "id": "mobile",
+        "label": "Mobile",
+        "description": "",
+        "submenu": [
+          {"id": "flexscan-22", "label": "Z+F FlexScan 22", "description": ""},
+          {"id": "heron", "label": "HERON [All parts will be included in this only]", "description": ""},
+          {"id": "gexcel-3d-forensics", "label": "Gexcel - - > 3D-FORENSICS", "description": ""}
+        ]
+      },
+      {
+        "id": "gpr",
+        "label": "GPR",
+        "description": "",
+        "submenu": [
+          {"id": "conquest-100", "label": "CONQUEST 100", "description": ""},
+          {"id": "lmx150-findar", "label": "LMX150™ FINDAR", "description": ""},
+          {"id": "noggin-ultra-100", "label": "NOGGIN® Ultra 100", "description": ""},
+          {"id": "noggin-100", "label": "Noggin 100", "description": ""},
+          {"id": "noggin-250", "label": "NOGGIN® 250", "description": ""},
+          {"id": "noggin-500", "label": "NOGGIN 500", "description": ""},
+          {"id": "noggin-1000", "label": "NOGGIN® 1000", "description": ""},
+          {"id": "pulseekko", "label": "pulseEKKO®", "description": ""},
+          {"id": "icemap", "label": "IceMap", "description": ""},
+          {"id": "rescue-radar", "label": "Rescue Radar", "description": ""}
+        ]
+      },
+      {
+        "id": "solutions",
+        "label": "Solutions",
+        "description": "",
+        "submenu": [
+          {"id": "opmms", "label": "OPMMS", "description": ""},
+          {"id": "iot-of-mines", "label": "IOT of Mines", "description": ""},
+          {"id": "airports", "label": "Airports", "description": ""}
+        ]
+      }
+    ]
+  }
+  
